8b98fc205865211d4cb16ed636052d96350c59a7
Ryosuke Kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2020-06-21 19:11:48 +0000

    update script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 dmm-map pca --config config/config_dkf01.json --dim 2
 dmm-map umap --config config/config_dkf01.json --dim 2 --data_sampling
 dmm-plot pca --config config/config_dkf01.json --input pca.jbl --x_plot_type heatmap --z_plot_type heatmap
+dmm-plot pca_s --config config/config_dkf01.json --input pca.jbl --x_plot_type heatmap --z_plot_type scatter
 dmm-plot umap --config config/config_dkf01.json --input umap.jbl --x_plot_type heatmap --z_plot_type heatmap
+dmm-plot umap_s --config config/config_dkf01.json --input umap.jbl --x_plot_type heatmap --z_plot_type scatter
+#dmm-plot umap --config config/config_dkf01.json --input umap.jbl --x_plot_type heatmap --z_plot_type heatmap --anim
+#dmm-plot umap_s --config config/config_dkf01.json --input umap.jbl --x_plot_type heatmap --z_plot_type scatter --anim
